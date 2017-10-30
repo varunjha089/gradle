a5f2299424d9e352f4b549a5710379466bb06d9d
Daniel Lacasse <daniel@gradle.com>
2017-10-29 20:51:26 -0400

    Fix configuration typo

--- subprojects/testing-native/src/integTest/groovy/org/gradle/nativeplatform/test/xctest/SwiftXcodeXCTestIntegrationTest.groovy
+++ subprojects/testing-native/src/integTest/groovy/org/gradle/nativeplatform/test/xctest/SwiftXcodeXCTestIntegrationTest.groovy
@@ -51,7 +51,7 @@ apply plugin: 'xctest'
 if (OperatingSystem.current().linux) {
     buildFile << """
     configurations {
-        testSwiftImportPath files('${xcTestImportPath}')
+        testSwiftImport files('${xcTestImportPath}')
         testNativeLink files('${xcTestLinkFile}')
         testNativeRuntime files('${xcTestRuntimeFile}')
     }
